8766751963ba8740177449f0785d5a97dd8288dc
Steven Barnhurst <stevenbarnhurst@gmail.com>
2014-11-29 16:16:21 +0900

    created working 3 way binding by replacing name key with new value

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,47 +10,34 @@
 var app = angular.module('workoutLogApp');
 
 app.controller('MainCtrl', ['$scope', '$firebase', function ($scope, $firebase) {
-  var ref = new Firebase("https://vkiw0dqfngi.firebaseio-demo.com/");
-  var ref2 = new Firebase("https://fiery-torch-1810.firebaseio.com/");
 
-  var sync = $firebase(ref2);
-  $scope.list1 = sync.$asArray();
+  var ref = new Firebase("https://fiery-torch-1810.firebaseio.com/");
+  var sync = $firebase(ref);
+  $scope.draggableObjects = sync.$asArray();
   function addItem(item){
-    $scope.list1.$add(item);
-    report(item);
-  }
-  $scope.messages = $firebase(ref).$asArray();
-
-  $scope.list5 = [
-    { 'title': 'Item 1', 'drag': true },
-    { 'title': 'Item 2', 'drag': true },
-    { 'title': 'Item 3', 'drag': true },
-    { 'title': 'Item 4', 'drag': true },
-    { 'title': 'Item 5', 'drag': true },
-    { 'title': 'Item 6', 'drag': true },
-    { 'title': 'Item 7', 'drag': true },
-    { 'title': 'Item 8', 'drag': true }
-  ];
-  function removeItem(index){
-    delete $scope.list1[index];
-  }
-  //ADD MESSAGE METHOD
-  $scope.addMessage = function(e) {
-
-    //LISTEN FOR RETURN KEY
-    if (e.keyCode === 13 && $scope.msg) {
-      //ALLOW CUSTOM OR ANONYMOUS USER NAMES
-      var name = $scope.name || 'anonymous';
-      $scope.messages.$add({from: name, body: $scope.msg});
-      //RESET MESSAGE
-      $scope.msg = "";
+    if(item){
+      $scope.draggableObjects.$add({name: item});
     }
   }
+
+  $scope.onDropComplete = function (drop, drag, evt) {
+    var dropItem = $scope.draggableObjects[drop];
+    var dragIndex = $scope.draggableObjects.indexOf(drag);
+    var holder = dropItem;
+    $scope.draggableObjects[drop] = drag;
+    $scope.draggableObjects[dragIndex] = holder;
+    $scope.draggableObjects.$save(drop).then(function(ref) {
+      console.log($scope.draggableObjects.indexOf(holder));// true
+    }, function(){
+      console.log('fail');// false
+    }); //save
+    $scope.draggableObjects.$save(dragIndex); //save
+  };
+
+
   function report(message){
     console.log(message);
   }
-
-  $scope.report = report;
   $scope.addItem = addItem;
-  $scope.removeItem = removeItem;
+  $scope.report = report;
 }]);
