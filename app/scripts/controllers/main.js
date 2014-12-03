74cb99d2ecd85a37c63202d94280dca0d61d17f0
Steven Barnhurst <stevenbarnhurst@gmail.com>
2014-12-03 10:10:57 +0900

    working sorter

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*global Firebase */
 /**
  * @ngdoc function
  * @name workoutLogApp.controller:MainCtrl
@@ -11,35 +11,28 @@ var app = angular.module('workoutLogApp');
 
 app.controller('MainCtrl', ['$scope', '$firebase', function ($scope, $firebase) {
 
-  var ref = new Firebase("https://fiery-torch-1810.firebaseio.com/");
+  var ref = new Firebase('https://fiery-torch-1810.firebaseio.com/');
   var sync = $firebase(ref);
-  $scope.draggableObjects = sync.$asArray();
+  $scope.fireData = sync.$asArray();
+
   function addItem(item){
     if(item){
-      $scope.draggableObjects.$add({name: item});
+      $scope.fireData.$add(['one','two','three']);
     }
   }
-
-  $scope.onDropComplete = function (drop, drag, evt) {
-    var dropItem = $scope.draggableObjects[drop];
-    var storeIndex = $scope.draggableObjects.indexOf(holder);
-    report(dropItem);
-    var dragIndex = $scope.draggableObjects.indexOf(drag);
-    var holder = dropItem;
-    $scope.draggableObjects[drop] = drag;
-    $scope.draggableObjects[dragIndex] = holder;
-    $scope.draggableObjects.$save(drop).then(function(ref) {
-      console.log($scope.draggableObjects.indexOf(holder));// true
-    }, function(){
-      console.log('fail');// false
-    }); //save
-    $scope.draggableObjects.$save(dragIndex); //save
-  };
-
-
+  function onDropComplete(index, item) {
+    var itemIndex = $scope.fireData[0].indexOf(item);
+    $scope.fireData[0].splice(itemIndex, 1);
+    $scope.fireData[0].splice(index, 0, item);
+    $scope.fireData.$save(0).then(function(ref) {
+      console.log(ref.key() === $scope.fireData[0].$id); // true
+    });
+  }
   function report(message){
     console.log(message);
   }
+
   $scope.addItem = addItem;
   $scope.report = report;
+  $scope.onDropComplete = onDropComplete;
 }]);
