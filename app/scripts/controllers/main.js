f905b2099d655a0de3828975a13ade74180d2057
Steven Barnhurst <stevenbarnhurst@gmail.com>
2014-12-04 19:46:05 +0900

    working copy with css simplification

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,19 +14,30 @@ app.controller('MainCtrl', ['$scope', '$firebase', function ($scope, $firebase) 
   var ref = new Firebase('https://fiery-torch-1810.firebaseio.com/');
   var sync = $firebase(ref);
   $scope.fireData = sync.$asArray();
+  $scope.draggableObjects = [{name:'one',isNew:true}, {name:'two',isNew:true},{name:'three',isNew:true}, {name:'four',isNew:true}];
 
+  //Use later to add workouts to main list
   function addItem(item){
     if(item){
-      $scope.fireData.$add(['one','two','three']);
+      $scope.fireData.$add([{name:'one',isNew:false}, {name:'two',isNew:false}, {name:'three',isNew:false}]);
     }
   }
   function onDropComplete(index, item) {
-    var itemIndex = $scope.fireData[0].indexOf(item);
-    $scope.fireData[0].splice(itemIndex, 1);
-    $scope.fireData[0].splice(index, 0, item);
-    $scope.fireData.$save(0).then(function(ref) {
-      console.log(ref.key() === $scope.fireData[0].$id); // true
-    });
+    if (item.isNew === false){
+      var itemIndex = $scope.fireData[0].indexOf(item);
+      $scope.fireData[0].splice(itemIndex, 1);
+      $scope.fireData[0].splice(index, 0, item);
+      $scope.fireData.$save(0).then(function(ref) {
+        console.log(ref.key() === $scope.fireData[0].$id); // true
+      });
+    } else if(item.isNew === true){
+      var newItem = angular.copy(item);
+      newItem.isNew = false;
+      $scope.fireData[0].splice(index, 0, newItem);
+      $scope.fireData.$save(0).then(function(ref) {
+        console.log(ref.key() === $scope.fireData[0].$id); // true
+      });
+    }
   }
   function report(message){
     console.log(message);
