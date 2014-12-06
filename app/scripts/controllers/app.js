dad14cfcbb1c53bb6fbe0a202197b8a6a979d6af
Steven Barnhurst <stevenbarnhurst@gmail.com>
2014-12-06 13:26:51 +0900

    fuck it

---
+++ app/scripts/controllers/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Shtav on 12/6/14.
+ */
+
+var app = angular.module('workoutLogApp');
+
+app.controller('appCtrl', ['$scope', function ($scope) {
+
+  function range(min, max, step) {
+    step = step || 1;
+    var input = [];
+    for (var i = min; i <= max; i += step) input.push(i);
+    return input;
+  }
+
+  $scope.range = range;
+}]);
