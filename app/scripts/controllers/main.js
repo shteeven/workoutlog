e0da26115c25d4533140be70e08258c81d3c56c8
Steven Barnhurst <stevenbarnhurst@gmail.com>
2014-12-07 19:38:08 +0900

    add logic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,16 +14,18 @@ app.controller('MainCtrl', ['$scope', '$firebase', function ($scope, $firebase) 
   var ref = new Firebase('https://fiery-torch-1810.firebaseio.com/');
   var sync = $firebase(ref);
   $scope.fireData = sync.$asArray();
-  $scope.draggableObjects = [{name:'one',isNew:true}, {name:'two',isNew:true},{name:'three',isNew:true}, {name:'four',isNew:true}];
+  $scope.draggableObjects = $scope.fireData[1];
 
   //Use later to add workouts to main list
   function addItem(item){
     if(item){
-      var obj = {name: item,isNew:false};
-      $scope.fireData[0].push(obj);
-      $scope.fireData.$save(0).then(function(ref) {
-        console.log(ref.key() === $scope.fireData[0].$id); // true
-      });
+      var obj = [
+        {name:'Standing Squat', type:'legs', completed: false, sets: 0, isNew:true},
+        {name:'Standing Squat', type:'legs', completed: false, sets: 0, isNew:true},
+        {name:'Standing Squat', type:'legs', completed: false, sets: 0, isNew:true},
+        {name:'Standing Squat', type:'legs', completed: false, sets: 0, isNew:true}
+      ];
+      $scope.fireData.$add(obj);
     }
   }
 
